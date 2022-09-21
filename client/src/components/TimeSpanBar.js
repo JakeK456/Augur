500accc7db0ebf289c8c92aec87c64970fed0a79
JK <jrileykruger@gmail.com>
2022-09-21 11:20:16 -0700

    added border under selected TimeSpanBar

--- client/src/components/TimeSpanBar.js
+++ client/src/components/TimeSpanBar.js
@@ -1,54 +1,74 @@
-export default function TimeSpanBar({ setTimeSpan }) {
+export default function TimeSpanBar({ timeSpan, setTimeSpan }) {
   const handleClickEvent = (event) => {
     if (event.target.type === "button") {
       setTimeSpan(event.target.textContent);
+
+      // event.currentTarget.childNodes.forEach((element) => {
+      //   element.firstChild.classList.remove("border-b-2");
+      // });
+
+      // event.target.classList.add("border-b-2");
     }
   };
 
   return (
     <div className="flex" onClick={handleClickEvent}>
-      <button
-        className="basis-[14.2857143%] shrink hover:bg-gray-200 text-sm px-1 border-r"
-        type="button"
-      >
-        1D
-      </button>
-      <button
-        className="basis-[14.2857143%] shrink hover:bg-gray-200 text-sm px-1 border-r"
-        type="button"
-      >
-        5D
-      </button>
-      <button
-        className="basis-[14.2857143%] shrink hover:bg-gray-200 text-sm px-1 border-r"
-        type="button"
-      >
-        1M
-      </button>
-      <button
-        className="basis-[14.2857143%] shrink hover:bg-gray-200 text-sm px-1 border-r"
-        type="button"
-      >
-        6M
-      </button>
-      <button
-        className="basis-[14.2857143%] shrink hover:bg-gray-200 text-sm px-1 border-r"
-        type="button"
-      >
-        1Y
-      </button>
-      <button
-        className="basis-[14.2857143%] shrink hover:bg-gray-200 text-sm px-1 border-r"
-        type="button"
-      >
-        5Y
-      </button>
-      <button
-        className="basis-[14.2857143%] shrink hover:bg-gray-200 text-sm px-1"
-        type="button"
-      >
-        Max
-      </button>
+      <div className="basis-[14.2857143%] shrink hover:bg-gray-200 px-1 border-r text-center">
+        <button
+          className={`${timeSpan === "1D" && "border-b-2"} w-8 text-sm`}
+          type="button"
+        >
+          1D
+        </button>
+      </div>
+      <div className="basis-[14.2857143%] shrink hover:bg-gray-200 px-1 border-r text-center">
+        <button
+          className={`${timeSpan === "5D" && "border-b-2"} w-8 text-sm`}
+          type="button"
+        >
+          5D
+        </button>
+      </div>
+      <div className="basis-[14.2857143%] shrink hover:bg-gray-200 px-1 border-r text-center">
+        <button
+          className={`${timeSpan === "1M" && "border-b-2"} w-8 text-sm`}
+          type="button"
+        >
+          1M
+        </button>
+      </div>
+      <div className="basis-[14.2857143%] shrink hover:bg-gray-200 px-1 border-r text-center">
+        <button
+          className={`${timeSpan === "6M" && "border-b-2"} w-8 text-sm`}
+          type="button"
+        >
+          6M
+        </button>
+      </div>
+      <div className="basis-[14.2857143%] shrink hover:bg-gray-200 px-1 border-r text-center">
+        <button
+          className={`${timeSpan === "1Y" && "border-b-2"} w-8 text-sm`}
+          type="button"
+        >
+          1Y
+        </button>
+      </div>
+      <div className="basis-[14.2857143%] shrink hover:bg-gray-200 px-1 border-r text-center">
+        <button
+          className={`${timeSpan === "5Y" && "border-b-2"} w-8 text-sm`}
+          type="button"
+        >
+          5Y
+        </button>
+      </div>
+      <div className="basis-[14.2857143%] shrink hover:bg-gray-200 px-1 text-center">
+        <button
+          className={`${timeSpan === "Max" && "border-b-2"} w-8 text-sm`}
+          type="button"
+        >
+          Max
+        </button>
+      </div>
     </div>
   );
 }
