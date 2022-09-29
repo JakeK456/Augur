57a79178219934fd8519067ba313718c85948e57
JK <jrileykruger@gmail.com>
2022-09-29 14:49:42 -0700

    added landing page guidance

--- client/src/components/Nav/WebHeader.js
+++ client/src/components/Nav/WebHeader.js
@@ -3,7 +3,9 @@ import { NavLink } from "react-router-dom";
 export default function WebHeader() {
   return (
     <div className="flex bg-primary text-secondary text-[40px] shadow-xl">
-      <h1 className="font-ubuntu m-2 pl-2">Augur</h1>
+      <a href="/" className="font-ubuntu m-2 pl-2">
+        Augur
+      </a>
       <div className="flex justify-end h-12 w-screen">
         <NavLink to="/predict" className="m-5 p-2">
           <p className="text-right text-[16px] font-maven hover:border-b-2 border-secondary">
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,30 @@
 import { useAuth } from "../util/auth";
+import { NavLink } from "react-router-dom";
 
 export default function Home() {
   const { isLoggedIn, user } = useAuth();
   return (
     <div>
       {/* TODO: display logged in user's username */}
-      <h1 className="text-center my-8 p-10 font-ubuntu text-[100px] text-primary">Welcome to Augur!</h1>
-      <p className="p-4 text-center font-maven text-[30px]">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo cumque
-        explicabo ipsum, facilis repellendus omnis amet in accusantium quisquam
-        nam qui consectetur sunt distinctio nemo molestiae ratione. Iure,
-        aliquam debitis.
-      </p>
+      <h1 className="text-center pt-10 font-ubuntu text-[80px] text-primary">
+        Augur
+      </h1>
+      <h3 className="text-center py-4 px-20 font-maven text-[30px] text-black">
+        - Your place to make, store, and share stock market predictions.
+      </h3>
+      <h3 className="text-center py-4 px-20 font-maven text-[20px] text-black">
+        What do you think the stock market will do next?
+      </h3>
+      <h3 className="text-center px-20 font-maven text-[20px] text-black">
+        Make a prediction now!
+      </h3>
+      <div className="flex justify-center m-10">
+        <NavLink to="/predict">
+          <button className="bg-green-600 rounded-full px-4 py-2 text-white hover:bg-green-500">
+            Get Started
+          </button>
+        </NavLink>
+      </div>
     </div>
   );
 }
